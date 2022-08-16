d22ac67693f421219e75ffd8b6df13dfb6fbcdd1
Abhijit5815 <mystiqfire@gmail.com>
2022-08-16 08:19:59 +0530

    add changes for first time in the dev branch created from main branch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            //We are able to comapre two objects
+
+            Console.WriteLine(t2.Equals(t1));
+            //.Equals will work even if comment out equal method in ticket class but we want to compare objects based upon
+            //duration hence we created our own method using the IEquatable
         }
     }
 }
